0f9ecec3f894b3e1606f45112709ff76e27dd8e9
Mukhamed Khadri <29703462+spartedo@users.noreply.github.com>
2019-11-04 18:40:50 +0300

    1.0.0 version

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # cm
+
+Welcome to the first Mukhamed's project - CinemaMachine!
+Links to me: [this](https://vk.com/khadri_m) and [this](https://twitter.com/spartedo)
---
+++ index.js
@@ -0,0 +1,11 @@
+// "Date","Price","Open","High","Low","Vol.","Change %"
+
+const MAX_INDEXES_AMOUNT = 0;
+const MAX_STOCK_PRICE = 0;
+const MAX_LOSE_YEAR = 0;
+const MAX_LOSE_MONTH = 0;
+
+let artists = require("./data/artists/1.json");
+let films = require("./data/films/1.json");
+
+console.log(films,artists["1"]);
