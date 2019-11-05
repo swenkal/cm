6360ccfe49fd7cb6133cf0fbd8a57915fa92bca6
Mukhamed Khadri <29703462+spartedo@users.noreply.github.com>
2019-11-05 13:31:10 +0300

    Console fixes

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const MAX_STOCK_PRICE = 0;
 const MAX_LOSE_YEAR = 0;
 const MAX_LOSE_MONTH = 0;
 
-let artists = require("./data/artists/1.json");
-let films = require("./data/films/1.json");
+let artists = [
+  require("./data/artists/1.json"), require("./data/artists/2.json"), require("./data/artists/3.json")
+];
 
-console.log(films,artists["1"]);
+let films = [
+  require("./data/films/1.json"), require("./data/films/2.json"), require("./data/films/3.json")
+];
+
+console.log(films[0]["director"]);
