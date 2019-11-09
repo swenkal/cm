8222fea2940dfd75cf72b6a944ed2344ebc63fb7
Mukhamed Khadri <29703462+spartedo@users.noreply.github.com>
2019-11-09 11:31:14 +0300

    Added readdir and catch error

--- index.js
+++ index.js
@@ -5,13 +5,34 @@ const MAX_STOCK_PRICE = 0;
 const MAX_LOSE_YEAR = 0;
 const MAX_LOSE_MONTH = 0;
 
-let arr = [];
-let arr2 = [];
-arr[0] = require("./data/films/1.json");
-arr[1] = require("./data/films/2.json");
-arr[2] = require("./data/films/3.json");
-arr2[0] = require("./data/artists/1.json");
-arr2[1] = require("./data/artists/2.json");
-arr2[2] = require("./data/artists/3.json");
+let movies = [];
+let artists = [];
+let fs = require('fs');
 
-console.log(`Movie:`, arr[2]["title"], `- one of the main actors:`, arr2[0]["name"]);
+movies[0] = require("./data/films/1.json");
+movies[1] = require("./data/films/2.json");
+movies[2] = require("./data/films/3.json");
+artists[0] = require("./data/artists/1.json");
+artists[1] = require("./data/artists/2.json");
+artists[2] = require("./data/artists/3.json");
+
+// console.log(`Movie:`, arr[2]["title"], `- one of the main actors:`, arr2[0]["name"]);
+//console.log(JSON.stringify(arr), JSON.stringify(arr2));
+
+fs.writeFile('data.json', JSON.stringify({movies, artists}), (err) => {
+  if (err) throw err;
+  console.log('The file has been saved!');
+});
+
+/*const data = new Uint8Array(Buffer.from('Hello Node.js'));
+fs.writeFile('message.txt', data, (err) => {
+  if (err) throw err;
+  console.log('The file has been saved!');
+});*/
+try {
+  films = fs.readdirSync("./data/films1");
+  artists = fs.readdirSync("./data/artists");
+  console.log(films, artists);
+} catch (e) {
+  console.log("Проблемки-фемки");
+}
