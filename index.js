62c18fab30fe7c34a1b6d1b709d3453efc93bbe4
Mukhamed Khadri <29703462+spartedo@users.noreply.github.com>
2019-11-12 23:40:07 +0300

    Updated index.js functions

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-// "Date","Price","Open","High","Low","Vol.","Change %"
-
-const MAX_INDEXES_AMOUNT = 0;
-const MAX_STOCK_PRICE = 0;
-const MAX_LOSE_YEAR = 0;
-const MAX_LOSE_MONTH = 0;
-
 let fs = require('fs');
 
-let films_names;
-let artists_names;
+function loadData() {
 
-try {
-  films_names = fs.readdirSync("./data/films");
-  artists_names = fs.readdirSync("./data/artists");
-  console.log(films, artists);
-} catch (e) {
-  console.log("Проблемки-фемки");
+  let films_names;
+  let artists_names;
+  let films = [];
+  let artists = [];
+
+  try {
+    films_names = fs.readdirSync("./data/films");
+    artists_names = fs.readdirSync("./data/artists");
+    console.log(films_names, artists_names);
+  } catch (e) {
+    console.log("Проблемки");
+  }
+
+  for (let filename of films_names) {
+    let film = JSON.parse(fs.readFileSync(`./data/films/${filename}`));
+    films.push(film);
+  }
+  for (filename of artists_names) {
+    let artist = JSON.parse(fs.readFileSync(`./data/artists/${filename}`));
+    artists.push(artist);
+  }
+  return {films, artists};
 }
 
-let films = [];
-let artists = [];
-
-for (let filename of films_names) {
-  let film = JSON.parse(fs.readFileSync(`./data/films/${filename}`));
-  films.push(film);
+function saveData(loadData) {
+  let loadedData = loadData();
+  fs.writeFile('data.json', JSON.stringify(loadedData), (e) => {
+    if (e) throw err;
+    console.log('The file has been saved!');
+  });
 }
 
-for (filename of artists_names) {
-  let artist = JSON.parse(fs.readFileSync(`./data/artists/${filename}`));
-  artists.push(artist);
-}
-
-fs.writeFile('data.json', JSON.stringify({movies, artists}), (e) => {
-  if (e) throw err;
-  console.log('The file has been saved!');
-});
+saveData(loadData);
