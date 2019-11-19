289694adcb870a4c735444809f4227cacea16525
swenkal <win-98@mail.ru>
2019-11-19 09:19:12 +0300

    Added package nodemon + handling request '//

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # cm
 
-Welcome to the first Mukhamed's project - CinemaMachine!
-Links to me: [this](https://vk.com/khadri_m) and [this](https://twitter.com/spartedo)
+Project for learn by Alex Podshivalov
--- index.js
+++ index.js
@@ -30,6 +30,9 @@ const http = require('http');
 const port = 3000;
 const requestHandler = (request, response) => {
     let requestedFile = decodeURI(request.url);
+    if (requestedFile === '/') {
+      requestedFile = '/index.html';
+    }
     response.setHeader('Content-Type', 'text/html; charset=utf-8;');
     console.log(requestedFile);
     try {
@@ -52,10 +55,10 @@ function saveData(films, artists) {
 // let loadedData = loadData();
 // saveData(loadedData.films, loadedData.artists);
 
-const server = http.createServer(requestHandler)
+const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
     if (err) {
-        return console.log('something bad happened', err)
+        return console.log('something bad happened', err);
     }    console.log(`server is listening on ${port}`)
 })
