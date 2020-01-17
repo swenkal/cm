51cbe0d0d9ad60db25f1a2650a2ab698c9bc633e
swenkal <win-98@mail.ru>
2020-01-17 13:03:21 +0300

    Added new api functions for URLs: create, delete, account, whoami.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let fs = require('fs');
 const mimeTypes = require('./config/mimeTypes.json');
 const PATH_TO_SESSIONS = "./data/sessions/";
 const TWO_DAYS_IN_MS = 172800000; //172800000 = 2 day * 24 hour * 60 min * 60 s * 1000 ms
+const PATH_TO_PROFILES = "./data/profiles/";
 
 function loadData() {
   const PATH_TO_FILMS = "./data/films/";
@@ -34,45 +35,48 @@ const http = require('http');
 const port = 3000;
 const requestHandler = (request, response) => {
 
+  if(businessLogicHandler(request, response))  return;
+
   let requestedFile = decodeURI(request.url);
   if (requestedFile.slice(-1) === '/') {
       requestedFile += 'index.html';
     }
 
-    const delimeteredFileName = requestedFile.split('.');
-    const fileExtension = delimeteredFileName[delimeteredFileName.length-1];
+  const fileExtension = getFileExtension(requestedFile);
 
-    let contentType = 'application/octet-stream';
-    if (typeof mimeTypes[fileExtension] !== 'undefined') {
+  let contentType = 'application/octet-stream';
+  if (typeof mimeTypes[fileExtension] !== 'undefined') {
       contentType = mimeTypes[fileExtension];
     }
-    console.log(fileExtension);
-    console.log(requestedFile);
-    if(businessLogicHandler(request, response))  return;
 
-    try {
-      let fileSizeInBytes = fs.statSync(`./web${requestedFile}`)['size'];
-      response.setHeader('Content-Length', `${fileSizeInBytes}`);
-      response.setHeader('Content-Type', `${contentType}`);
-    //  let fileContent = fs.readFileSync(`./web${requestedFile}`);
-      let contentRequestedFile = new fs.ReadStream(`./web${requestedFile}`);
-      contentRequestedFile.pipe(response);
-      contentRequestedFile.on('error', (err) => {
-        response.setHeader('Content-Type', 'text/html; charset=utf-8;');
-        response.statusCode = 500;
-        response.end("Server Error");
-        console.error(err);
-      });
-      response.on('close', () => {
-        contentRequestedFile.destroy();
-      });
-      response.statusCode = 200;
-    } catch (e) {
-      console.log(e.message);
+  try {
+    let fileSizeInBytes = fs.statSync(`./web${requestedFile}`)['size'];
+    response.setHeader('Content-Length', `${fileSizeInBytes}`);
+    response.setHeader('Content-Type', `${contentType}`);
+    let contentRequestedFile = new fs.ReadStream(`./web${requestedFile}`);
+    contentRequestedFile.pipe(response);
+    contentRequestedFile.on('error', (err) => {
       response.setHeader('Content-Type', 'text/html; charset=utf-8;');
-      response.statusCode = 404;
-      response.end('Запрашиваемого файла не существует');
-    }
+      response.statusCode = 500;
+      response.end("Server Error");
+      console.error(err);
+    });
+    response.on('close', () => {
+      contentRequestedFile.destroy();
+    });
+    response.statusCode = 200;
+  } catch (e) {
+    console.log(e.message);
+    response.setHeader('Content-Type', 'text/html; charset=utf-8;');
+    response.statusCode = 404;
+    response.end('Запрашиваемого файла не существует');
+  }
+}
+
+function getFileExtension(fileName){
+  let delimeteredFileName = fileName.split('.');
+  let fileExtension = delimeteredFileName[delimeteredFileName.length-1];
+  return fileExtension;
 }
 
 function businessLogicHandler(request, response){
@@ -82,15 +86,18 @@ function businessLogicHandler(request, response){
   if(!apiRegExp.test(requestedUrl)) return false;
 
   let sessionContext = getSessionContext(request);
-
+  console.log(`sessionContext: ${JSON.stringify(sessionContext)}`);
   collectPostData(request, (requestParams) => {
     let router =
       [
         { urlPatern: /^\/api$/ , handler: indexApiHandler},
         { urlPatern: /^\/api\/auth/ , handler: authorisation},
-        { urlPatern: /^\/api\/whoami$/ , handler: checkUserSession}
+        { urlPatern: /^\/api\/whoami$/ , handler: getCurrentUser},
+        { urlPatern: /^\/api\/account$/ , handler: getUserAccount},
+        { urlPatern: /^\/api\/create$/ , handler: createUser},
+        { urlPatern: /^\/api\/delete$/ , handler: deleteUser}
       ];
-    for(location of router){
+    for(let location of router){
       console.log(`${location.urlPatern} vs ${requestedUrl}  ${location.urlPatern.test(requestedUrl)}`);
       if(location.urlPatern.test(requestedUrl)) {
         location.handler(request, response, requestParams, sessionContext);
@@ -109,19 +116,19 @@ function getSessionContext(request){
   let userCookie = parseCookie(request.headers.cookie);
   let resultContext = {};
   if(userCookie['token'] == 'undefined' || typeof userCookie['token'] !== 'string') return resultContext;
-
+  let dirSessionName = getDirNameForUserToken(userCookie['token']);
   try {
-    let tokenContent = JSON.parse(fs.readFileSync(`${PATH_TO_SESSIONS}${userCookie['token']}.json`));
+    let tokenContent = JSON.parse(fs.readFileSync(`${PATH_TO_SESSIONS}${dirSessionName}${userCookie['token']}.json`));
     if (tokenContent.expiresTime < Date.now()) {
       fs.unlink(`${PATH_TO_SESSIONS}${userCookie['token']}.json`, (err) => {
         if (err) console.error(`Something wrong with deleting sessionFile ${userCookie['token']}: ${err}`);
-        console.log('Old session file was deleted');
+        console.log(`Old session file ${userCookie['token']} was deleted`);
       });
       return resultContext;
     }
 
     tokenContent.expiresTime = Date.now() + TWO_DAYS_IN_MS;
-    fs.writeFile(`${PATH_TO_SESSIONS}${userCookie['token']}.json`, JSON.stringify(tokenContent), (err) => {
+    fs.writeFile(`${PATH_TO_SESSIONS}${dirSessionName}${userCookie['token']}.json`, JSON.stringify(tokenContent), (err) => {
       if (err) console.error(`Can't update session file ${userCookie['token']}: ${err}`);
       console.log(`Session file ${userCookie['token']} updated`);
     });
@@ -145,15 +152,18 @@ function collectPostData (request, callback) {
   });
 
   request.on('end', () => {
-    callback(parsePostParams(postData));
+    let decodedPostData = decodeURIComponent(postData);
+    console.log(`Func collectPostData ${decodedPostData}`);
+    callback(parsePostParams(decodedPostData);
     return
   });
 }
 
 function parsePostParams(postData){
   let requestParams = {};
+  let decodedPostData = decodeURI(postData);
   if (postData)
-    for (let keyValue of postData.split("&")) {
+    for (let keyValue of decodedPostData.split("&")) {
       let [key, value] = keyValue.split("=");
       requestParams[key] = value;
   }
@@ -168,7 +178,7 @@ function indexApiHandler(request, response, postData, sessionContext){
   response.end(JSON.stringify(indexApiResult));
 }
 
-function checkUserSession(request, response, postData, sessionContext){
+function getCurrentUser(request, response, postData, sessionContext){
     let resultCheckSession = {};
     response.setHeader('Content-Type', 'application/json');
     if('username' in sessionContext) {
@@ -182,7 +192,6 @@ function checkUserSession(request, response, postData, sessionContext){
 }
 
 function authorisation(request, response, postData, sessionContext) {
-    const PATH_TO_PROFILES = "./data/profiles/";
     let resultCheckUser = {
         authed: false
     };
@@ -207,9 +216,9 @@ function authorisation(request, response, postData, sessionContext) {
       if (err) {
         response.statusCode = 401;
       } else {
-          let userDataObject = JSON.parse(data);
-          if(userDataObject.account.password == postData.password){
-            console.log(parseCookie(request.headers.cookie));
+          let userInfo = JSON.parse(data);
+          if(userInfo.account.password == postData.password){
+            console.log(parseCookie(request.headers.cookie)); //TODO: Add checkSession for user
             resultCheckUser.authed = generateSession(response, postData);
           } else {
             response.statusCode = 401;
@@ -233,7 +242,7 @@ function generateSession(response, postData){
   let sessionЕxpiresTime = sessionCreateTime + TWO_DAYS_IN_MS;
   let cookieLifeTime = new Date(sessionЕxpiresTime).toGMTString();
   let userToken = generateRandomString();
-  let dirForUserToken = getDirNameForSessionToken(userToken);
+  let dirForUserToken = getDirNameForUserToken(userToken);
 
   let sessionData = {};
   sessionData.login = postData.login;
@@ -251,7 +260,18 @@ function generateSession(response, postData){
   }
 }
 
-function getDirNameForSessionToken(userToken){
+function generateRandomString(length){
+   let lengthString = length || 16;
+   let resultString = '';
+   let randNum;
+   while( lengthString-- ){
+     randNum = Math.floor(Math.random() * 62);
+     resultString += String.fromCharCode(randNum + (randNum < 10 ? 48 : randNum < 36 ? 55 : 61))
+   }
+   return resultString;
+}
+
+function getDirNameForUserToken(userToken){
   let dirName = `${userToken.substr(0,2)}/`;
   if(!(fs.existsSync(`${PATH_TO_SESSIONS}${dirName}`))){
     try{
@@ -264,32 +284,180 @@ function getDirNameForSessionToken(userToken){
   return dirName;
 }
 
+function getUserAccount(request, response, postData, sessionContext){
+    resultUserAccount = {};
+    response.setHeader("Content-Type", "application/json");
+    if(!("username" in sessionContext)){
+      response.statusCode = 403;
+      resultUserAccount.error = 'You must log in. Follow to /api/auth';
+      response.end(JSON.stringify(resultUserAccount));
+      return;
+    }
 
-function generateRandomString(length){
-   let lengthString = length || 16;
-   let resultString = '';
-   let randNum;
-   while( lengthString-- ){
-     randNum = Math.floor(Math.random() * 62);
-     resultString += String.fromCharCode(randNum + (randNum < 10 ? 48 : randNum < 36 ? 55 : 61))
-   }
-   return resultString;
+    fs.readFile(`${PATH_TO_PROFILES}${sessionContext.username}.json`,'utf-8', function(err, data){
+      if(err){
+        console.log(`Error read userInfo ${sessionContext}: ${err}`);
+        response.statusCode = 500;
+        resultUserAccount.error = `Sorry. We can't read user info.`;
+      } else {
+        let userInfo = JSON.parse(data);
+        resultUserAccount = userInfo.account;
+        response.statusCode = 200;
+      }
+      response.end(JSON.stringify(resultUserAccount));
+    });
 }
 
-let checkingSessionDirs = false;
-setInterval(() => {
-  if(checkingSessionDirs) return;
-  checkSessionDirs()}, 40000);
+function createUser(request, response, postData, sessionContext){
+    let resultCreateUser = {};
+    response.setHeader("Content-Type", "application/json");
 
-function checkSessionDirs(){
-  checkingSessionDirs = true;
-  let sessionDirNames = fs.readdirSync(PATH_TO_SESSIONS);
-  console.log(sessionDirNames);
-  for (let currentSessionDir of sessionDirNames){
-      deleteOldSessionFiles(`${PATH_TO_SESSIONS}${currentSessionDir}/`);
-      deleteEmptySessionDir(`${PATH_TO_SESSIONS}${currentSessionDir}/`);
+    if(!("username" in sessionContext)){
+      response.statusCode = 401;
+      resultCreateUser.created = false;
+      response.end(JSON.stringify(resultCreateUser));
+      return;
+    }
+    if(isEmptyObject(postData)){
+      response.statusCode = 400;
+      resultCreateUser.created = false;
+      response.end(JSON.stringify(resultCreateUser));
+      return;
+    }
+    let parsedPostData = postData;
+    //TODO: functions checkNewLogin() and checkNewPassword()
+    if(!("login" in parsedPostData) || !("password" in parsedPostData)){
+      response.statusCode = 400;
+      resultCreateUser.created = false;
+      response.end(JSON.stringify(resultCreateUser));
+      return;
+    }
+    let listCreatedUsers = fs.readdirSync(PATH_TO_PROFILES);
+    if(listCreatedUsers.includes(parsedPostData.login)){
+      response.statusCode = 400;
+      resultCreateUser.created = false;
+      response.end(JSON.stringify(resultCreateUser));
+      return;
+    }
+    fs.readFile(`${PATH_TO_PROFILES}${sessionContext.username}.json`,'utf-8', function(err, data){
+      if(err){
+        response.statusCode = 500;
+        console.log(`createUser: Can't read user file: ${err}`);
+        resultCreateUser.created = false;
+        response.end(JSON.stringify(resultCreateUser));
+        return;
+      }
+      let adminInfo = JSON.parse(data);
+      if (adminInfo.account.role !== "admin"){
+        response.statusCode = 403;
+        resultCreateUser.created = false;
+        response.end(JSON.stringify(resultCreateUser));
+      } else {
+          let newUserInfo = {}; //TODO: rename!!!
+          newUserInfo.account = {};
+          newUserInfo.account.name = parsedPostData.name;
+          newUserInfo.account.lastname = parsedPostData.lastname;
+          newUserInfo.account.mail = parsedPostData.mail;
+          newUserInfo.account.role = parsedPostData.role;
+          newUserInfo.account.password = parsedPostData.password;
+
+          fs.writeFile(`${PATH_TO_PROFILES}${parsedPostData.login}.json`, JSON.stringify(newUserInfo), function(err){
+            if(err){
+              console.log(`createUser: Write file error ${parsedPostData.login}: ${err} `);
+              response.statusCode = 500;
+              resultCreateUser.created = false
+            } else {
+              response.statusCode = 200;
+              resultCreateUser.created = true;
+            }
+            response.end(JSON.stringify(resultCreateUser));
+          });
+        }
+    });
+}
+
+
+function deleteUser(request, response, postData, sessionContext){
+  let resultDeleteUser = {};
+  resultDeleteUser.deleted = false;
+  response.setHeader("Content-Type", "application/json");
+
+  if(!("username" in sessionContext)){
+    response.statusCode = 401;
+    response.end(JSON.stringify(resultDeleteUser));
+    return;
   }
-  checkingSessionDirs = false;
+
+  if(isEmptyObject(postData) || !("login" in postData)){
+    response.statusCode = 400;
+    response.end(JSON.stringify(resultDeleteUser));
+    return;
+  }
+
+  if(postData.login == sessionContext.username){
+    response.statusCode = 406;
+    response.end(JSON.stringify(resultDeleteUser));
+    return;
+  }
+
+  fs.readFile(`${PATH_TO_PROFILES}${sessionContext.username}.json`, 'utf-8', function(err, data){
+    if(err){
+      console.log(`Delete user: Read admin property error: ${err}`);
+      response.statusCode = 500;
+      response.end(JSON.stringify(resultDeleteUser));
+      return;
+    }
+    let adminInfo = JSON.parse(data);
+    if(adminInfo.account.role !== "admin"){
+      response.statusCode = 403;
+      response.end(JSON.stringify(resultDeleteUser));
+    } else {
+        fs.unlink(`${PATH_TO_PROFILES}${postData.login}.json`, function(err){
+          if(err){
+            console.log(`Delete user error: ${err}`);
+            response.statusCode = 500;
+            response.end(JSON.stringify(resultDeleteUser));
+            return;
+          }
+          resultDeleteUser.deleted = true;
+          response.end(JSON.stringify(resultDeleteUser));
+        });
+    }
+  });
+}
+
+
+function isEmptyObject(someObj){
+  for(let key in someObj){
+    return false;
+  }
+  return true;
+}
+
+/*
+let idSessionCleaner = startSessionCleaner();
+setTimeout(() => {
+  stopSessionCleaner(idSessionCleaner);
+}, 80000);*/
+
+
+function startSessionCleaner(){
+  let timeCheckOldSessions = 0;
+  let idSessionCleaner = setInterval(() => {
+    if( timeCheckOldSessions < 0) return;
+    timeCheckOldSessions++;
+    if( timeCheckOldSessions >= 60) {
+     timeCheckOldSessions = -1;
+     let sessionDirNames = fs.readdirSync(PATH_TO_SESSIONS);
+     console.log(sessionDirNames);
+     for (let currentSessionDir of sessionDirNames){
+         deleteOldSessionFiles(`${PATH_TO_SESSIONS}${currentSessionDir}/`);
+         deleteEmptySessionDir(`${PATH_TO_SESSIONS}${currentSessionDir}/`);
+     }
+      timeCheckOldSessions = 0;
+    }
+  }, 1000);
+  return idSessionCleaner;
 }
 
 function deleteOldSessionFiles(pathToSessionDir) {
@@ -320,6 +488,10 @@ function deleteEmptySessionDir(pathToSessionDir){
   }
 }
 
+function stopSessionCleaner(idSessionCleaner){
+   clearInterval(idSessionCleaner);
+   console.log(`Session cleaner was stoped.`);
+}
 
 const server = http.createServer(requestHandler);
 server.listen(port, (err) => {
@@ -329,6 +501,21 @@ server.listen(port, (err) => {
 })
 
 /*OLD BULLSHIT
+setInterval(() => {
+  if( timeCheckOldSessions < 0) return;
+   timeCheckOldSessions++;
+  if( timeCheckOldSessions >= 60) {
+   timeCheckOldSessions = -1;
+   let sessionDirNames = fs.readdirSync(PATH_TO_SESSIONS);
+   console.log(sessionDirNames);
+   for (let currentSessionDir of sessionDirNames){
+       deleteOldSessionFiles(`${PATH_TO_SESSIONS}${currentSessionDir}/`);
+       deleteEmptySessionDir(`${PATH_TO_SESSIONS}${currentSessionDir}/`);
+   }
+    timeCheckOldSessions = 0;
+  }}, 1000);
+
+
 function getRequestParams(url) {
   let resultParams = {}
   let [adress, stringParams] = url.split("?");
